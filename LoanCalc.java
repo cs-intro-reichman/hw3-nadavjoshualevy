6129746420ef65d5fe2963dfb68741cdc6fb03fd
Nadav Joshua Levy <nadavjoshualevy@Mac-2.lan>
2025-11-17 18:27:02 +0200

    2.4

--- LoanCalc.java
+++ LoanCalc.java
@@ -43,7 +43,7 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		iterationCounter = 1;
+		iterationCounter = 0;
 		double increment = 0.001;
 		double g = loan/n;
 
